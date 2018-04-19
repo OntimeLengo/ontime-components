21ec34d76ad188235c3204d020107e57ee05e1ea
OntimeLengo <ontime.lengo@gmail.com>
2018-04-19 22:07:49 +0200

    Added Icon component

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { PureComponent, PropTypes } from '../../libs';
+
+/**
+ * Class Icon
+ * 
+ * @author Dmytro Hotovskyi
+ * @created 28/03/2018
+ */
+export default class Icon extends PureComponent {
+
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+    size: PropTypes.string,
+    onClick: PropTypes.func
+  }
+
+  onClick(e) {
+    this.props.onClick && this.props.onClick(e);
+  }
+
+  render() {
+    let extraClasses = {};
+
+    extraClasses.fa = true;
+    extraClasses['fa-' + this.props.value] = true;
+    this.props.size && (extraClasses['fa-' + this.props.size] = true);
+
+    return (
+      <i 
+        className={ this.className('ontime-icon', extraClasses) } 
+        style={ this.style() }
+        onClick={ this.onClick.bind(this) }
+      />
+    );
+  }
+
+}
